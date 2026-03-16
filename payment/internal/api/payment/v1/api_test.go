b91d4c7531b227d05ab334260f315bd65e533061
zhenklchhh <zaleshchenok.e@mail.ru>
2026-03-16 12:18:27 +0300

    feat: payment api unit tests

---
+++ payment/internal/api/payment/v1/api_test.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/zhenklchhh/KozProject/payment/internal/model"
+	paymentV1 "github.com/zhenklchhh/KozProject/shared/pkg/proto/payment/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type testCase struct {
+	req          *paymentV1.PayOrderRequest
+	expectedUUID string
+	expectedErr  error
+}
+
+func (s *ApiSuit) TestPayOrderValidRequest() {
+	tc := &testCase{
+		req: &paymentV1.PayOrderRequest{
+			OrderUuid:     gofakeit.UUID(),
+			UserUuid:      gofakeit.UUID(),
+			PaymentMethod: paymentV1.PaymentMethod_PAYMENT_METHOD_CARD,
+		},
+		expectedUUID: gofakeit.UUID(),
+	}
+	convertedReq := &model.PayOrderRequest{
+		OrderUuid:     tc.req.OrderUuid,
+		UserUuid:      tc.req.UserUuid,
+		PaymentMethod: model.PaymentMethodCard,
+	}
+	s.service.On("PayOrder", s.ctx, convertedReq).Return(&model.PayOrderResponse{TransactionUuid: tc.expectedUUID}, nil)
+	response, err := s.handler.PayOrder(s.ctx, tc.req)
+	s.Require().Equal(tc.expectedUUID, response.TransactionUuid)
+	s.Require().NoError(err)
+}
+
+func (s *ApiSuit) TestPayOrderInvalidRequest() {
+	tc := &testCase{
+		req: &paymentV1.PayOrderRequest{
+			OrderUuid:     "",
+			UserUuid:      "",
+			PaymentMethod: 404,
+		},
+		expectedErr: status.Errorf(codes.InvalidArgument, "payment service: validation error"),
+	}
+	// The validation happens before service call, so no service call should be made
+	response, err := s.handler.PayOrder(s.ctx, tc.req)
+	s.Require().Error(err)
+	s.Require().Equal(tc.expectedErr, err)
+	s.Require().Nil(response)
+}
---
+++ payment/internal/api/payment/v1/suit_test.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	serviceMocks "github.com/zhenklchhh/KozProject/payment/internal/service/mocks"
+)
+
+type ApiSuit struct {
+	suite.Suite
+
+	ctx     context.Context
+	service *serviceMocks.PaymentService
+	handler *api
+}
+
+func (s *ApiSuit) SetupTest() {
+	s.ctx = context.Background()
+	s.service = serviceMocks.NewPaymentService(s.T())
+	s.handler = NewApi(s.service)
+}
+
+func (s *ApiSuit) TearDownTest() {
+}
+
+func TestApiIntegration(t *testing.T) {
+	suite.Run(t, new(ApiSuit))
+}
